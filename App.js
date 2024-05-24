6945cffc85c0db38672848aa8903a7929c02fb0d
akashrajum7 <akashrajum7@gmail.com>
2024-05-24 20:41:54 +0530

    feat: Refactor App.js to use nested elements

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Hello from React!"
-);
+/*
+<div id="parent">
+  <div id="child">
+    <h1>Hello World</h1>
+    <h2>React is awesome</h2>
+  </div>
+  <div id="sibling">
+    <h1>Hello World</h1>
+    <h2>React is awesome</h2>
+  </div>
+</div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", {}, "Hello World"),
+      React.createElement("h2", {}, "React is awesome"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    {
+      id: "sibling",
+    },
+    [
+      React.createElement("h1", {}, "Hello World"),
+      React.createElement("h2", {}, "React is awesome"),
+    ]
+  ),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
